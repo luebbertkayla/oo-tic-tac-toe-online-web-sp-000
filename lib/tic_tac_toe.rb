ff2b2d05aa8e509d6d399366f24fe6fca80c33cb
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-22 21:27:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,20 +2,10 @@
 class TicTacToe 
   attr_accessor :board
   
+  
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
-  
-  WIN_COMBINATIONS =[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,4,8],
-    [2,4,6],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    ]
     
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
